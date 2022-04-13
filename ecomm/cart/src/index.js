52545764c6f4b30e1e08fa86ec1457408fc89a5b
Yeasin Arafath <arafath.yeasin1019@gmail.com>
2022-04-14 04:13:00 +0600

    Module Federation Implementation.

---
+++ ecomm/cart/src/index.js
@@ -0,0 +1,3 @@
+const cartText = `<div>You have 100 items in your cart</div>`;
+
+document.querySelector('#dev-cart').innerHTML = cartText;
--- ecomm/cart/webpack.config.js
+++ ecomm/cart/webpack.config.js
@@ -2,17 +2,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
-    mode: "development",
-    devServer: {
-        port: 8080,
+    mode : "development",
+    devServer : {
+        port : 8082,
     },
-    plugins: [
+    plugins:[
         new ModuleFederationPlugin({
-            name: 'conatiner',
-            remotes: {
-                products: 'products@http://localhost:8081/remoteEntry.js',
-                cart: 'cart@http://localhost:8082/remoteEntry.js',
-            },
+            name: 'cart',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './CartShow' : './src/index.js'
+            }
         }),
         new HtmlWebpackPlugin({
             template: './public/index.html',
