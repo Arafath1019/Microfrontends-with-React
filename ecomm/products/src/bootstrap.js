c30218702372caece02603ae43945126e726e048
Yeasin Arafath <arafath.yeasin1019@gmail.com>
2022-05-22 13:58:37 +0600

    Final Touch

--- ecomm/products/src/bootstrap.js
+++ ecomm/products/src/bootstrap.js
@@ -1,40 +1,16 @@
-// import faker from "faker";
-
-// let products = "";
-
-// for (let i = 0; i<3; i++){
-//     const name = faker.commerce.productName();
-//     products += `<div>${name}</div>`;
-// }
-
-
-
 const mount = (el) =>{
     products = `<div>Mobile</div><div>Monitor</div><div>Computer</div>`
 
     el.innerHTML = products;    
 };
 
-// Context/Situation #1
-// We are running this file in development in isolation
-// We are using our local index.html file
-// Which DEFINETELY has an element with an id of 'dev-product'
-// We want to immediately render our app into that element
 if (process.env.NODE_ENV === 'development'){
     const el = document.querySelector('#dev-products');
 
-    // Assuming our container doesn't have an element with id 'dev-products'
-    if (el){
+   if (el){
         mount(el);
     }
 } 
-
-
-// Context/Situation #2
-// We are running this file in development or production
-// through the CONTAINER app
-// NO GUARANTEE that an element with an id of 'dev-products'
-// We DO NOT WANT try to immediately render the app
 
 export { mount };
 
--- mfp/packages/marketing/src/App.js
+++ mfp/packages/marketing/src/App.js
@@ -1,11 +1,13 @@
 import { createGenerateClassName, StylesProvider } from "@material-ui/core/styles";
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'ma',
 });
+
 
 export default () => {
     return <div>
